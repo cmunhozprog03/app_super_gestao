319773fb11286013fa9a020b796273d9227f9de8
cmunhozprog03 <cmunhozprog03@hotmail.com>
2021-10-05 13:01:49 -0300

    Sessão 6 completa

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,38 @@ use Illuminate\Support\Facades\Route;
 
 //
 
-Route::get('/', 'PrincipalController@principal');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('login', function(){ return 'Login'; })->name('site.login');
 
-Route::get('/sobre-nos', 'SobreNosController@sobreNos');
+Route::prefix('/app')->group(function(){
+    Route::get('clientes', function(){ return 'Cliente'; })->name('app.clientes');
+    Route::get('fornecedores', function(){ return 'Fornecedores'; })->name('app.fornecedores');
+    Route::get('produtos', function(){ return 'Produtos'; })->name('app.produtos');
+});
 
-Route::get('/contato', 'ContatoController@contato');
+Route::get('/rota1', function(){
+    echo 'rota 1';
+})->name('site.rota1');
+
+//Route::redirect('/rota2', '/rota1');
+
+Route::get('/rota2', function(){
+     return redirect()->route('site.rota1');
+ });
+
+ Route::fallback(function(){
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">Clique aqui</a> para acessar a página inicial';
+ });
+
+
+
+//expressões regulares para rotas
+// Route::get('contato/{nome}/{categoria_id}',
+// function(
+//     string $nome = 'Desconhecido',
+//     int $categoria_id = 1
+//     ){
+//     echo "Estamos aqui: $nome - $categoria_id ";
+// })->where('categoria_id', '[0-9]+')->where('nome','[A-Za-z]+');
