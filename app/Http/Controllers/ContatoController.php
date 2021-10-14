b6b7cab1fef92ed91e2848b087f42cad4dbc4305
cmunhozprog03 <cmunhozprog03@hotmail.com>
2021-10-14 15:27:43 -0300

    Gravar dados fill, create e aray associativo

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,13 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\SiteContato;
 use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
-    public function contato()
+    public function contato(Request $request)
     {
-        var_dump($_POST);
+        // $contato = new SiteContato();
+
+        // $contato->nome = $request->input('nome');
+        // $contato->telefone = $request->input('telefone');
+        // $contato->email = $request->input('email');
+        // $contato->motivo_contato = $request->input('motivo_contato');
+        // $contato->mensagem = $request->input('mensagem');
+
+        //$contato->save();
+
+
+        $contato = new SiteContato();
+        $contato->create($request->all());
+        //$contato->fill($request->all());
+        //$contato->save();
+
+        //print_r($contato->getAttributes());
+
         return view('site.contato', ['titulo' => 'Contato teste']);
     }
 }
